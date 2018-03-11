55cab943e0159d00ae092d940f0d8b4247479247
yuta_koga <yutakoga1002@gmail.com>
2018-03-12 06:28:41 +0900

    2018031220180312

--- hajiboot/hajiboot/src/main/java/com/example/hajiboot/HajibootApplication.java
+++ hajiboot/hajiboot/src/main/java/com/example/hajiboot/HajibootApplication.java
@@ -2,6 +2,7 @@ package com.example.hajiboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,16 @@ public class HajibootApplication {
 
 	@GetMapping("/")
 	String home() {
-		return "Hello Spring Boot";
+		return "Hello World!"/* + " (" + System.getenv("CF_INSTANCE_INDEX") + ") Ver.2"*/;
 	}
+	
+	@GetMapping("kill")
+	void kill() {
+		System.exit(1);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HajibootApplication.class, args);
+//		System.out.println(new Pbkdf2PasswordEncoder().encode("demo"));
 	}
 }
